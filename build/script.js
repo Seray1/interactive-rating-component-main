c897c49b1e30fd91375eebe7be4dcbefe175cee7
Seray1 <107807594+Seray1@users.noreply.github.com>
2023-05-29 15:36:01 +0100

    update

--- build/script.js
+++ build/script.js
@@ -1,11 +1,14 @@
 // Get all the rating number elements
-const stars = document.querySelectorAll("div")
+const stars = document.querySelectorAll("label")
 const submitBtn = document.getElementById("submit-btn")
 const selected = document.getElementById("selected")
+const frontCard = document.getElementById("front-card")
+const backCard = document.getElementById("back-card")
 
 // Add click event listeners to each rating number
 stars.forEach((number, index) => {
   number.addEventListener("click", () => {
+    selectedRating = index + 1
     // Remove the 'selected' class from all numbers
     console.log("clicked")
     stars.forEach((number) => {
@@ -20,4 +23,10 @@ stars.forEach((number, index) => {
   })
 })
 
-console.log(stars)
+submitBtn.addEventListener("click", () => {
+  selected.textContent = `You selected ${selectedRating} here out of 5`
+  frontCard.classList.add("hidden")
+  backCard.classList.remove("hidden")
+  backCard.classList.add("flex")
+  backCard.classList.add("animate__fadeInDown")
+})
